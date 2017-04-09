0e2a0327f51a30b313dd99113457c4d59be26885
Qin Lyu <qlyu@g.harvard.edu>
2017-04-09 13:03:43 -0400

    data definition

--- include/data.h
+++ include/data.h
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+
+enum fea_type {
+  CONT = 0,
+  DISC = 1,
+  RANK = 2
+};
 
 union value {
-  float v;
-  size_t id;
+  float v; //type 0, continuous feature
+  size_t id; // type 1, discrete feature
+  int n; //type 2, rank feature
 };
+
+struct DabData {
+  std::vector<fea_type> types;
+  std::vector<std::vector<value>> values;
+};
+
