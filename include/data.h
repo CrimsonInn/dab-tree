65e597c4ac32e481a1966ddf0bed0aa8a2680302
vol <xiabingfeng@gmail.com>
2017-04-09 15:16:30 -0400

    add protobuf frame for dabTree

--- include/data.h
+++ include/data.h
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <vector>
 
-enum fea_type {
+enum FeaType {
   CONT = 0,
   DISC = 1,
   RANK = 2
 };
 
-union value {
+union Value {
   float v; //type 0, continuous feature
   size_t id; // type 1, discrete feature
   int n; //type 2, rank feature
 };
 
 struct DabData {
-  std::vector<fea_type> types;
-  std::vector<std::vector<value>> values;
+  std::vector<FeaType> types;
+  std::vector<std::vector<Value>> values;
 };
 
