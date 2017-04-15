3a2dc56da4e6601256f052ad519fa9e50ace14fd
vol <xiabingfeng@gmail.com>
2017-04-14 23:55:32 -0400

    add batch data protobuf read and write API

--- src/protobuf/read_proto.cc
+++ src/protobuf/read_proto.cc
@@ -7,28 +7,96 @@
 #include <glog/logging.h>
 
 
-void read_batch_data() {
+BatchPtr read_batch_data(const std::string& file_name) {
 
   // Verify that the version of the protobuf that we linked against is
   // compatible with the version of the headers we compiled against.
-  // GOOGLE_PROTOBUF_VERIFY_VERSION;
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
   LOG(INFO) << "Begin read batch data ...";
-  std::cout << "hehe" << std::endl;
 
-  return;
+  BatchPtr batch = std::make_shared<Batch>();
+  data::Batch read_batch_data;
+
+  // Read the existing address book.
+  std::fstream input(file_name, std::ios::in | std::ios::binary);
+  if (!input) {
+    LOG(INFO) << file_name << ": File not found. Creating a new file.";
+  } else if (!read_batch_data.ParseFromIstream(&input)) {
+    LOG(ERROR) << "Failed to parse batch data.";
+    return NULL;
+  }
+
+  // TODO: check clos == samples_size 
+
+  // batch->rows = read_batch_data.rows();
+  // batch->clos = read_batch_data.cols();
+  unsigned int clos = read_batch_data.cols();
+  for (size_t i = 0; i < read_batch_data.fea_types_size(); ++i) {
+    batch->fea_types.push_back(FeaType(read_batch_data.fea_types(i)));
+  }
+  // TODO: do we need to use ceil() ???
+  for (size_t i = 0; i < read_batch_data.samples_size()/clos; ++i) {
+    std::vector<Value> sample;
+    for (size_t j = 0; j < clos; ++j) {
+        if (batch->fea_types[j] == CONT){
+          sample.push_back({.v = read_batch_data.samples(i * clos + j).v()});
+        } else if (batch->fea_types[j] == DISC) {
+          sample.push_back({.cls = read_batch_data.samples(i * clos + j).id()});
+        } else if (batch->fea_types[j] == RANK) {
+          // TODO: check variable format
+          sample.push_back({.n = static_cast<int>(read_batch_data.samples(i * clos + j).n())});
+        } else {
+          LOG(ERROR) << "Batch data type error.";
+          return NULL;
+        }
+    }
+    batch->samples.push_back(sample);
+  }
+
+  return batch;
+
 }
+
 
 void read_tree() {
 	return;
 }
 
-//int main(int argc, char const *argv[])
-//{
-//	google::InitGoogleLogging(argv[0]);
-//    //FLAGS_log_dir = "./log";
-//	LOG(INFO) << "Begin read batch data ...";
-//	/* code */
-//	std::cout << "Hi" <<std::endl;
-//	// GLOG_logtostderr=1;
-//	return 0;
-//}
+
+void print_batch_data(BatchPtr batch) {
+  for (auto fea : batch->fea_types) {
+    std::cout << FeaType(fea) ;
+  }
+  std::cout << std::endl;
+
+  unsigned int cols = batch->fea_types.size();
+  unsigned int total = batch->samples.size();
+  for (auto sample : batch->samples) {
+    for (size_t j = 0; j < cols; ++j) {
+      if (batch->fea_types[j] == CONT){
+        std::cout << sample[j].v ;
+      } else if (batch->fea_types[j] == DISC) {
+        std::cout << sample[j].cls ;
+      } else if (batch->fea_types[j] == RANK) {
+        std::cout << sample[j].n ;
+      } else {
+        LOG(ERROR) << "Batch data type error.";
+      return ;
+      }
+    }
+    std::cout << std::endl;
+  }
+  return ;
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  std::string file_name = "BATCH_DATA_FILE";
+
+  BatchPtr batch = std::make_shared<Batch>();
+  batch = read_batch_data(file_name);
+  print_batch_data(batch);
+
+  return 0;
+}
--- src/protobuf/write_proto.cc
+++ src/protobuf/write_proto.cc
@@ -6,7 +6,76 @@
 #include "tree.h"
 #include <glog/logging.h>
 
-void write_batch_data() {
+bool write_batch_data(Batch &batch_data, const std::string& file_name) {
+
+  LOG(INFO) << "Begin write batch data ...";
+
+  google::uint32 fea_type_nums = batch_data.fea_types.size();
+  google::uint32 value_rows = batch_data.samples.size();
+  google::uint32 value_cols = 0;
+  if(value_rows > 0){
+  	value_cols = batch_data.samples[0].size();
+  }
+
+  // TODO: do we need to check fea_type_nums ?= value_rows?
+
+  // Verify that the version of the protobuf that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  data::Batch write_batch_data;
+
+  // Read the existing address book.
+  std::fstream input(file_name, std::ios::in | std::ios::binary);
+  if (!input) {
+    LOG(INFO) << file_name << ": File not found. Creating a new file.";
+  } else if (!write_batch_data.ParseFromIstream(&input)) {
+    LOG(ERROR) << "Failed to parse batch data.";
+    return false;
+  }
+
+  // write bach data into protobuf
+  write_batch_data.set_rows(value_rows);
+  write_batch_data.set_cols(value_cols);
+
+  for (auto fea_type : batch_data.fea_types) {
+  	write_batch_data.add_fea_types(data::FeaType(fea_type));
+  	// data::FeaType* new_fea_type = write_batch_data.add_fea_types();
+  	// (* new_fea_type) = batch_data.fea_types[i];
+  }
+
+  for (auto sample_row : batch_data.samples) {
+  	for (size_t i = 0; i < sample_row.size(); ++i) {
+  	//for (auto sample : sample_row) {
+  		// data::Value *new_value = write_batch_data.add_samples();
+  		LOG(INFO) << batch_data.fea_types[i] ;
+  		if (batch_data.fea_types[i] == CONT){
+  			write_batch_data.add_samples()->set_v(sample_row[i].v);
+  			//new_value->set_v(float(sample_row[i]));
+  		} else if (batch_data.fea_types[i] == DISC) {
+  			write_batch_data.add_samples()->set_id(sample_row[i].cls);
+  		} else if (batch_data.fea_types[i] == RANK) {
+  			write_batch_data.add_samples()->set_n(sample_row[i].n);
+  		} else {
+  			LOG(ERROR) << "Batch data type error.";
+  			return false;
+  		}
+  	}
+  }
+
+	// Write the new address book back to disk.
+  std::fstream output(file_name, std::ios::out | std::ios::trunc | std::ios::binary);
+  if (!write_batch_data.SerializeToOstream(&output)) {
+    LOG(ERROR) << "Failed to write batch data.";
+    return false;
+  } else {
+    LOG(INFO) << "Success!";
+  }
+
+  // Optional:  Delete all global objects allocated by libprotobuf.
+  google::protobuf::ShutdownProtobufLibrary();
+
+  return true;
 
 }
 
@@ -14,8 +83,25 @@ void write_tree() {
 
 }
 
-//int main(int argc, char const *argv[])
-//{
-//	/* code */
-//	return 0;
-//}
+int main(int argc, char const *argv[])
+{
+  /* code */
+  std::string file_name = "BATCH_DATA_FILE";
+  Batch batch_data;
+
+  batch_data.fea_types.push_back(CONT);
+  batch_data.fea_types.push_back(DISC);
+  batch_data.fea_types.push_back(RANK);
+
+  std::vector<Value> sample = {{.v=1}, {.cls=0}, {.n=2}};
+  batch_data.samples.push_back(sample);
+
+  // for (auto sample : batch_data.samples) {
+  // 	for (auto s : sample) {
+  // 		LOG(INFO) << s. ;
+  // 	}
+  // }
+
+  write_batch_data(batch_data, file_name);
+  return 0;
+}
