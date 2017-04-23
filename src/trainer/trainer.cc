a3e7f7afb825a4463711321bb5286188333d879b
Qin Lyu <qlyu@g.harvard.edu>
2017-04-23 19:30:39 -0400

    .

--- src/trainer/trainer.cc
+++ src/trainer/trainer.cc
@@ -1,13 +1,24 @@
 #include "matrix.h"
 #include "data.h"
 #include "tree.h"
+#include "proto_func.h"
 #include <iostream>
-
+#include <string>
 
 class Trainer {
 public:
+  Trainer(const std::string proto_name) {
+    ptr = read_batch_data(proto_name);
+    tree = std::make_shared<RegTree>();
+    tree->SetType(ptr->get_fea_types());
+    tree->TrainOneTree(ptr->get_samples(), 1.0);
+    tree->Print();
+  }
+
+  if(ptr->Next(batch))
 
 
 private:
+  BatchPtr ptr;
   RegTree tree;
 };
