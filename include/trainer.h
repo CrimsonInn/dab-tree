13c53d9141e8f6a81dbcc34f55c7b672f502b029
Qin Lyu <qlyu@g.harvard.edu>
2017-04-28 13:20:22 -0400

    validation finished

--- include/trainer.h
+++ include/trainer.h
@@ -18,6 +18,27 @@ public:
     step_size = 0.1;
   }
   
+  void Validate() {
+    auto batch_ptr = dp.get_validation();
+    VectorPtr result_ptr = std::make_shared<std::vector<float>>();
+    batch_ptr->SetType(0, FeaType::DISC);
+    tree.Predict(batch_ptr, result_ptr);
+    
+    float loss = 0.0;
+    float err = 0;
+    for (size_t i = 0; i < batch_ptr->GetHeight(); ++i) {
+      float y = (*batch_ptr)(i, 0).cls == 0? -1 : 1;
+      float margin = (*result_ptr)[i] * y;
+      //      float tmp = margin < 1? 1-margin: 0;
+      float tmp = std::exp(-margin);
+      loss += tmp;
+      if (margin < 0)
+        err += 1;
+    }
+    LOG(INFO) << "Validation Loss=" << loss/result_ptr->size() << ", Accuracy=" << 1-err*1.0/result_ptr->size();
+
+  }
+  
   void TrainOneBatch() {
     dp.get_next_batch(batch_ptr, batch_size);
     VectorPtr result_ptr = std::make_shared<std::vector<float>>();
@@ -43,9 +64,10 @@ public:
       if (margin < 0)
         err += 1;
     }
-    LOG(INFO) << "Loss=" << loss/batch_ptr->GetHeight() << ", err rate=" << err/batch_ptr->GetHeight();
+    LOG(INFO) << "Training Loss=" << loss/batch_ptr->GetHeight() << ", Accuracy=" << 1-err*1.0/batch_ptr->GetHeight();
     batch_ptr->SetType(0, FeaType::CONT);
     tree.TrainOneTree(batch_ptr, step_size);
+    if (tree.NumTrees() % 10 == 0) Validate();
     step_size /= 1;
   }
   
