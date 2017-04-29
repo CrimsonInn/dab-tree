fc044f031c8b62e14c78833131b8ac4c3ecac4e5
Qin Lyu <qlyu@g.harvard.edu>
2017-04-29 09:25:27 -0400

    linux environment fix

--- build/cmake.sh
+++ build/cmake.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cmake -DCMAKE_BUILD_TYPE=Release -DUSE_MPI=ON ..
+#cmake -DCMAKE_BUILD_TYPE=Release -DUSE_MPI=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
--- include/trainer.h
+++ include/trainer.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <cmath>
 #include <thread>
+#include <algorithm>
 
 class Trainer {
 public:
